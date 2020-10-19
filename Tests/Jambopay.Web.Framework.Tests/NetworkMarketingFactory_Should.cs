5040ef9909c29af2bfec232bcaec0f92c023c231
Ezra Mungai <ezra.mungai@sibasi.ltd>
2020-10-19 13:22:44 +0300

    Add tests

--- Tests/Jambopay.Web.Framework.Tests/NetworkMarketingFactory_Should.cs
+++ Tests/Jambopay.Web.Framework.Tests/NetworkMarketingFactory_Should.cs
@@ -6,9 +6,18 @@ namespace Jambopay.Web.Framework.Tests
     [TestClass]
     public class NetworkMarketingFactory_Should
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateAmbassadorCommission()
         {
+            var networkMaketingFactory = new NetworkMarketingFactory(null, null, null, null);
+
+            var transactionAmount = 100m;
+            var commissionRate = .05m;
+
+            var ambassadorCommission = networkMaketingFactory.ComputeAmbassadorCommission(transactionAmount, commissionRate);
+
+            Assert.AreEqual(5m, ambassadorCommission);
         }
     }
 }
